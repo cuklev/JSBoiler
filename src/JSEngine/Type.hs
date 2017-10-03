5fab3ebb9cb3f4f58a509f4bf07b5897c9573258
Kristiyan Tsaklev <cuki@cukii.tk>
2017-10-04 01:48:22 +0300

    Cleanup code

--- src/JSEngine/Type.hs
+++ src/JSEngine/Type.hs
@@ -1,43 +1,32 @@
 module JSEngine.Type where
 
 import Data.IORef
+import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
-data Property = Property { value :: JSType
-                         , writeable :: Bool
-                         , get :: IO JSType
-                         , set :: JSType -> IO ()
-                         }
-
-type ObjectProperties = M.Map String Property
 
 data JSType = JSNumber Double
             | JSString String
             | JSBoolean Bool
             | JSUndefined
             | JSNull
-            | JSObject ObjectProperties
-            | JSFunction { object :: IORef ObjectProperties
-                         , this :: IORef ObjectProperties
-                         , stack :: Stack
-                         , function :: [JSType] -> IO JSType
+            | JSObject (IORef Object)
+
+data Object = Object { properties :: Map String Property
+                     , behaviour :: Maybe Function
+                     , prototype :: Maybe Object
+                     }
+
+data Property = Property { value :: JSType
+                         , writeable :: Bool
+                         , enumerable :: Bool
+                         , configurable :: Bool
+                         , get :: Maybe Function
+                         , set :: Maybe Function
                          }
 
-data JSBinding = JSBinding { isConst :: Bool
-                           , valueJS :: IORef JSType
-                           }
-
-type Stack = [M.Map String JSBinding]
-
-lookupBinding :: String -> Stack -> Maybe JSBinding
-lookupBinding _ [] = Nothing
-lookupBinding name (x:xs) = maybe (lookupBinding name xs) Just $ M.lookup name x
-
-instance Show JSType where
-    show (JSNumber x) = show x
-    show (JSString x) = x
-    show (JSBoolean x) = if x then "true" else "false"
-    show JSNull = "null"
-    show JSUndefined = "undefined"
-    show (JSObject _) = "[object Object]" -- Hmm
-    show (JSFunction _ _ _ _) = "function() { /* implementation */ }" -- Obviously not finished
+data Function = Function { boundThis :: Maybe Object
+                         , functionScope :: Stack
+                         , argumentNames :: [String]
+                         , function :: [JSType] -> Stack -> IO JSType
+                         }
