95f84fcd199af432eb6103f04493f41a6dd8850b
Kristiyan Tsaklev <cuki@cukii.tk>
2017-10-05 00:46:17 +0300

    Add getProperty and setProperty functions

--- src/JSEngine/Type.hs
+++ src/JSEngine/Type.hs
@@ -4,6 +4,8 @@ import Data.IORef
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
+import JSEngine.Stack
+
 
 data JSType = JSNumber Double
             | JSString String
@@ -26,7 +28,20 @@ data Property = Property { value :: JSType
                          }
 
 data Function = Function { boundThis :: Maybe Object
-                         , functionScope :: Stack
+                         , functionScope :: Stack JSType
                          , argumentNames :: [String]
-                         , function :: [JSType] -> Stack -> IO JSType
+                         , function :: [JSType] -> Stack JSType -> IO JSType
                          }
+
+
+getProperty :: String -> Object -> Maybe Property
+getProperty name obj =
+            let own = M.lookup name $ properties obj
+            in case own of
+                Nothing -> prototype obj >>= getProperty name
+                _ -> own
+
+setProperty :: String -> Property -> Object -> Object
+setProperty name prop obj =
+            let m = M.insert name prop $ properties obj
+            in obj { properties = m }
